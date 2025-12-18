a4f48752125a66872f58f82353ebd41bfb65008f
Axenide <axenide@gmail.com>
2025-12-18 02:15:23 -0300

    added something...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/newshell': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  }
+});
