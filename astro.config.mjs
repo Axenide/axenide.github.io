d48e2980d41f086b9b1ed07f1abb67bad56293a3
Axenide <axenide@gmail.com>
2025-12-30 02:14:19 -0300

    zola porting

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  redirects: {
-    '/newshell': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-  }
-});
